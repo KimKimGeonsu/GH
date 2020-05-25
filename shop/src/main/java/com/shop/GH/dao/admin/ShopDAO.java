a92420818c34d232d46c20d773e3598d087a2d45
kimkimgeonsu <rjstn193390@gmail.com>
2020-05-25 18:21:15 +0900

    geonsu[qqqqqqqqqqqqq];

--- shop/src/main/java/com/shop/GH/dao/admin/ShopDAO.java
+++ shop/src/main/java/com/shop/GH/dao/admin/ShopDAO.java
@@ -1,5 +1,11 @@
 package com.shop.GH.dao.admin;
 
+import java.util.List;
+
+import com.shop.GH.vo.admin.ShopVO;
+
 public interface ShopDAO {
+	
+	public List<ShopVO> listShop();
 
 }
--- shop/src/main/java/com/shop/GH/dao/admin/ShopDAOImpl.java
+++ shop/src/main/java/com/shop/GH/dao/admin/ShopDAOImpl.java
@@ -1,14 +1,30 @@
 package com.shop.GH.dao.admin;
 
+import java.util.List;
+
 import javax.inject.Inject;
 
 import org.mybatis.spring.SqlSessionTemplate;
 import org.springframework.stereotype.Repository;
+
+import com.shop.GH.vo.admin.ShopVO;
+
 
 @Repository
 public class ShopDAOImpl implements ShopDAO{
 	
 	@Inject
 	private SqlSessionTemplate sql;
+
+	
+	
+	/**
+	 * @상품목록
+	 */
+	@Override
+	public List<ShopVO> listShop() {
+		// TODO Auto-generated method stub
+		return sql.selectList("shopAdmin.shopList");
+	}
 
 }
